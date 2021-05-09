00e1fe4c0af3e9967931de7e3219b02563f72a3f
yeguacelestial <yeguacelestial@gmail.com>
2021-05-08 22:09:57 -0500

    Dia 7: Guardar citas en localStorage, utilizar useEffect y lanzar proyecto en netlify

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,9 +1,26 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario'
+import Cita from './components/Cita'
 
 function App() {
+
+  // Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'))
+  if (!citasIniciales) {
+    citasIniciales = []
+  }
+
   // Arreglo de citas
-  const [citas, guardarCitas] = useState([])
+  const [citas, guardarCitas] = useState(citasIniciales)
+
+  // Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect( () => {
+    if (citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]))
+    }
+  }, [citas, citasIniciales])
 
   // Función que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
@@ -12,6 +29,14 @@ function App() {
       cita
     ])
   }
+
+  // Función que elimina la cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id)
+    guardarCitas(nuevasCitas)
+  }
+
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'
 
   return (
     <Fragment>
@@ -26,7 +51,14 @@ function App() {
           </div>
 
           <div className="one-half column">
-            2
+            <h2>{titulo}</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
           </div>
         </div>
       </div>
